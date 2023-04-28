7f988a644f5c760ef4911d51e20dc45e5d510eae
Daniel Vitoriano <daniel.costaht2@gmail.com>
2023-04-28 17:15:11 -0300

    corrigido imagens quebradas

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ import imagemClickEsperto from "./Assets/images/clickEsperto.webp";
 import imagemKennyNoBreu from "./Assets/images/kennynobreu.png";
 import imagemVisaDXS from "./Assets/images/visadxs_play.png";
 import imagemFruitHunt from "./Assets/images/fruithunt.png";
+import imagemDcv from "./Assets/images/dcv.jpeg";
+import imagemFan from "./Assets/images/fan.png";
+import imagemBeachpark from "./Assets/images/Beach-Park-logo.png";
+import imagemUnity from "./Assets/images/unity3d_30t70u3b.jpg";
+import imagemLia from "./Assets/images/lia.jpeg";
+import imagemGodot from "./Assets/images/godot.jpeg";
+import imageFbuni from "./Assets/images/centro-universitario-farias-brito_logo.png";
 
 function App() {
   const [anos, setAnos] = useState();
@@ -38,7 +45,7 @@ function App() {
         <div id="bg-perfil"></div>
         <div id="base-foto-e-nome">
             <div id="foto-e-nome">
-                <div id="foto-de-perfil"><img src="https://media.licdn.com/dms/image/D4D03AQF29j0bwkz9Iw/profile-displayphoto-shrink_200_200/0/1668779323875?e=1678320000&v=beta&t=3e4uzEkSh8DKtvZV0YYqeVOufXbiUTUs2wpUUeliR9E" alt='Daniel Vitoriano'></img></div>
+                <div id="foto-de-perfil"><img src={imagemDcv} alt='Daniel Vitoriano'></img></div>
                 <div id="nome-perfil"><h2 id="nome">Daniel Vitoriano</h2></div>
             </div>
         </div>
@@ -97,11 +104,11 @@ function App() {
             <Projeto relacionados={
               [{
                 link: "https://www.linkedin.com/company/fan-studios/", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQHNnBZerw-YPw/company-logo_200_200/0/1519896941933?e=1681344000&v=beta&t=MmI1fyzGE5_6xUOV1ttrWOI1LTL0XYMZZw0aSq1gPJ0"
+                foto: imagemFan
               },
               {
                 link: "https://www.linkedin.com/company/beach-park-hot-is-e-turismo-s-a/", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQHFlgLvr_CzBw/company-logo_200_200/0/1600960251007?e=1681344000&v=beta&t=ELUZ_oU7bXrHUDQVKC4rQBtvJXc8QNyMZ1CYfpfzhK4"
+                foto: imagemBeachpark
               },
               {
                 link: "https://pt-br.reactjs.org", 
@@ -119,11 +126,11 @@ function App() {
             <Projeto relacionados={
               [{
                 link: "https://www.linkedin.com/company/fan-studios/", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQHNnBZerw-YPw/company-logo_200_200/0/1519896941933?e=1681344000&v=beta&t=MmI1fyzGE5_6xUOV1ttrWOI1LTL0XYMZZw0aSq1gPJ0"
+                foto: imagemFan
               },
               {
                 link: "https://www.linkedin.com/company/beach-park-hot-is-e-turismo-s-a/", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQHFlgLvr_CzBw/company-logo_200_200/0/1600960251007?e=1681344000&v=beta&t=ELUZ_oU7bXrHUDQVKC4rQBtvJXc8QNyMZ1CYfpfzhK4"
+                foto: imagemBeachpark
               },
               {
                 link: "https://phaser.io", 
@@ -141,7 +148,7 @@ function App() {
             <Projeto relacionados={
               [{
                 link: "https://www.linkedin.com/company/fan-studios/", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQHNnBZerw-YPw/company-logo_200_200/0/1519896941933?e=1681344000&v=beta&t=MmI1fyzGE5_6xUOV1ttrWOI1LTL0XYMZZw0aSq1gPJ0"
+                foto: imagemFan
               },
               {
                 link: "https://www.linkedin.com/company/copel/", 
@@ -149,7 +156,7 @@ function App() {
               },
               {
                 link: "https://unity.com", 
-                foto:"https://media.licdn.com/dms/image/C560BAQHEIxCwW404IA/company-logo_200_200/0/1633446722058?e=1681344000&v=beta&t=Ix7hjghNBrJ9KPJ4V0219utk2bVzrbrZ8x5u4kvRtnc"
+                foto: imagemUnity
               }
               ]
               }
@@ -163,7 +170,7 @@ function App() {
             <Projeto relacionados={
               [{
                 link: "https://www.linkedin.com/company/fan-studios/", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQHNnBZerw-YPw/company-logo_200_200/0/1519896941933?e=1681344000&v=beta&t=MmI1fyzGE5_6xUOV1ttrWOI1LTL0XYMZZw0aSq1gPJ0"
+                foto: imagemFan
               },
               {
                 link: "https://www.linkedin.com/company/copel/", 
@@ -171,7 +178,7 @@ function App() {
               },
               {
                 link: "https://unity.com", 
-                foto:"https://media.licdn.com/dms/image/C560BAQHEIxCwW404IA/company-logo_200_200/0/1633446722058?e=1681344000&v=beta&t=Ix7hjghNBrJ9KPJ4V0219utk2bVzrbrZ8x5u4kvRtnc"
+                foto: imagemUnity
               }
               ]
               }
@@ -185,7 +192,7 @@ function App() {
             <Projeto relacionados={
               [{
                 link: "https://www.linkedin.com/in/liagodoy/", 
-                foto:"https://media-exp1.licdn.com/dms/image/C4D03AQHaDNvwEs--sQ/profile-displayphoto-shrink_200_200/0/1652889752766?e=1674086400&v=beta&t=o2xId53IZET_BXw1zBdpA_s8RPVLE9LZMEK6PlW6ZSc"
+                foto: imagemLia
               },
               {
                 link: "https://www.youtube.com/c/CrieSeusJogos/videos", 
@@ -193,7 +200,7 @@ function App() {
               },
               {
                 link: "https://unity.com", 
-                foto:"https://media.licdn.com/dms/image/C560BAQHEIxCwW404IA/company-logo_200_200/0/1633446722058?e=1681344000&v=beta&t=Ix7hjghNBrJ9KPJ4V0219utk2bVzrbrZ8x5u4kvRtnc"
+                foto: imagemUnity
               }
               ]
               }
@@ -207,7 +214,7 @@ function App() {
             <Projeto relacionados={
               [{
                 link: "https://www.linkedin.com/in/liagodoy/", 
-                foto:"https://media-exp1.licdn.com/dms/image/C4D03AQHaDNvwEs--sQ/profile-displayphoto-shrink_200_200/0/1652889752766?e=1674086400&v=beta&t=o2xId53IZET_BXw1zBdpA_s8RPVLE9LZMEK6PlW6ZSc"
+                foto: imagemLia
               },
               {
                 link: "https://becocultural.com.br/lei-aldir-blanc/", 
@@ -219,7 +226,7 @@ function App() {
               },
               {
                 link: "https://godotengine.org", 
-                foto:"https://media.licdn.com/dms/image/C4E0BAQGt35gt5oH1DA/company-logo_200_200/0/1533663863337?e=1681344000&v=beta&t=wmgPjjYV0pjuQtocbyxrEHTWAMWUZXlWflZKqSODxk0"
+                foto: imagemGodot
               }
               ]
               }
@@ -232,11 +239,11 @@ function App() {
             <Projeto relacionados={
             [{
               link: "https://www.linkedin.com/school/centro-universitario-farias-brito/", 
-              foto:"https://media.licdn.com/dms/image/C4E0BAQFWKiEHH2Cg8w/company-logo_200_200/0/1519866211119?e=1681948800&v=beta&t=r2VSuUFjzwT4H0vMSY8wtJaNTT4Zx1EPGyngg8cEWP8"
+              foto: imageFbuni
             },
             {
               link: "https://godotengine.org", 
-              foto:"https://media.licdn.com/dms/image/C4E0BAQGt35gt5oH1DA/company-logo_200_200/0/1533663863337?e=1681344000&v=beta&t=wmgPjjYV0pjuQtocbyxrEHTWAMWUZXlWflZKqSODxk0"
+              foto: imagemGodot
             }
             ]
             }
